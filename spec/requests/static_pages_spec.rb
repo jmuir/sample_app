c486cf03851aa78e4cdaf0e321eaea4bbbd558c3
Johan Muir <jmuir@verticalresponse.com>
2013-12-27 17:21:06 -0600

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe "Static Pages" do
+  describe "Home page" do
+
+    it "should have the right Home page title" do
+      visit '/static_pages/home'
+      page.should have_selector('title', :text => " | Home")
+    end
+  end
+
+  describe "Help page" do
+    it "should have the right Help page title" do
+      visit '/static_pages/help'
+      page.should have_selector('title', :text => " | Help")
+    end
+  end
+
+  describe "About page" do
+    it "should have the right About Us page title" do
+      visit '/static_pages/about'
+      page.should have_selector('title', :text => " | About Us")
+    end
+  end
+end
+
