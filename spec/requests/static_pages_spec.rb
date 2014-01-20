9de04b0470c382919f369429f4049a6dfe89a4dc
Johan Muir <jmuir@verticalresponse.com>
2014-01-20 17:43:59 -0600

    Finish layout and routes. Add user controller and initial specs

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,31 +2,53 @@ require 'spec_helper'
 
 describe "Static Pages" do
   describe "Home page" do
+    before { visit root_path }
 
     it "should have the right Home page title" do
-      visit '/static_pages/home'
-      page.should have_selector('title', :text => " | Home")
+      page.should have_selector('title', text: " | Home")
+    end
+
+    it "should have the base title" do
+      page.should have_selector('title',
+      text: "Ruby on Rails Tutorial Sample App")
+    end
+
+
+    it "should have the h1 'Sample App'" do
+      page.should have_selector('h1', text: 'Sample App')
     end
   end
 
   describe "Help page" do
+    before { visit help_path }
     it "should have the right Help page title" do
-      visit '/static_pages/help'
-      page.should have_selector('title', :text => " | Help")
+      page.should have_selector('title', text: " | Help")
+    end
+
+    it "should have the h1 'Help'" do
+      page.should have_selector('h1', text: "Help")
     end
   end
 
   describe "About page" do
+    before { visit about_path }
     it "should have the right About Us page title" do
-      visit '/static_pages/about'
       page.should have_selector('title', :text => " | About Us")
+    end
+
+    it "should have the h1 'About'" do
+      page.should have_selector('h1', text: "About")
     end
   end
 
   describe "Contact page" do
+    before { visit contact_path }
     it "should have the right Contact Us page title" do
-      visit '/static_pages/contact'
       page.should have_selector('title', :text => " | Contact Us")
+    end
+
+    it "should have the h1 'Contact'" do
+      page.should have_selector('h1', text: "Contact")
     end
   end
 end
