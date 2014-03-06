2cac2ab2b401c77a3cec7614be25de97cca937bc
Johan Muir <jmuir@verticalresponse.com>
2014-03-05 18:19:37 -0600

    Adding more validations and password migration to user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,13 +13,15 @@ require 'spec_helper'
 
 describe User do
   before do 
-    @user = User.new( email: "test@testemail.com", name: "Foo Bar" )
+    @user = User.new( email: "test@testemail.com", name: "Foo Bar", password: "foobar", password_confirmation: "foobar")
   end
 
   subject{@user}
 
   it { should respond_to(:email) }
   it { should respond_to(:name) }
+  it { should respond_to(:password_digest) }
+  it{ should respond_to(:password_confirmation) }
 
   it { should be_valid }
 
@@ -39,6 +41,52 @@ describe User do
     before { @user.name = "a" * 51 }
 
     it { should_not be_valid }
+  end
+
+  describe "when email format is invalid" do
+    it "should be invalid" do
+      addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com]
+
+      addresses.each do |invalid_address|
+        @user.email = invalid_address
+        @user.should_not be_valid
+      end
+    end
+  end
+
+  describe "when email format is valid" do
+    it "should be valid" do
+      addresses = %w[user@foo.com user-foor@bar.org A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn user@foo.COM]
+
+      addresses.each do |valid_address|
+        @user.email = valid_address
+        @user.should be_valid
+      end
+    end
+  end
+  
+  describe "when email address already exists on database" do
+    before do
+      user_with_same_email_address = @user.dup
+
+      user_with_same_email_address.email = @user.email.upcase
+
+      user_with_same_email_address.save
+    end
+
+    it {should_not be_valid}
+  end
+
+  describe "when password is not present" do
+    before {@user.password = @user.password_confirmation = " "}
+
+    it {should_not be_valid}
+  end
+
+  describe "when password and confirmation password don't match" do
+    before {@user.password_confirmation = "mismatch"}
+
+    it {should_not be_valid}
   end
 
 end
