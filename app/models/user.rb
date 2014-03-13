a8b4240298d37237667c7990c43f4d9288b45b82
Johan Muir <jmuir@verticalresponse.com>
2014-03-12 18:02:46 -0600

    Adding rspecs for password confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
+  attr_accessible :email, :name, :password, :password_confirmation
 
   before_save { |user| user.email = email.downcase }
 
@@ -19,4 +19,6 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+
+  validates :password, presence: true, length: { minimum: 6 }
 end
