30645c54d92280777f14a4b732f012e63ebf2e04
Johan Muir <jmuir@verticalresponse.com>
2014-06-18 11:28:55 -0600

    Adding final steps for user sign_up

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -27,7 +27,7 @@ describe "User Pages" do
 
     describe "with invalid information" do
       it "should not create a user" do
-        expect {Click_button submit}.not_to change(User, :count)
+        expect { click_button submit }.not_to change(User, :count)
       end
     end
 
@@ -40,7 +40,7 @@ describe "User Pages" do
       end
 
       it "should create a user" do
-        expect {Click_button submit}.to change(User, :count).by(1)
+        expect { click_button submit }.to change(User, :count).by(1)
       end
     end
   end
